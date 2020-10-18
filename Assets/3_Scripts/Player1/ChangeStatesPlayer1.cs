be43baab3114ed60fa2f21792b0722c99a1f6276
Matias Badaloni <53199112+MatiasBadaloni@users.noreply.github.com>
2020-10-17 21:44:39 -0300

    Musica terminada

---
+++ Assets/3_Scripts/Player1/ChangeStatesPlayer1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChangeStatesPlayer1 : MonoBehaviour
+{
+    //Atrivutos y Propiedaes
+    private bool skill;
+    public bool changeSkill;
+    public List<GameObject> objectWhite;
+    public List<GameObject> objectBlack;
+
+    //Activar Habilidad
+    private void Start()
+    {
+        if(changeSkill == false)
+        {
+            skill = true;
+        }
+        SelectObject();
+        ChangeStates();
+    }
+   
+    public void ChangeStates()
+    {
+        if (skill == true)
+        {
+            foreach (var obj in objectWhite)
+            {
+                obj.SetActive(true);
+            }
+
+            foreach (var obj in objectBlack)
+            {
+                obj.SetActive(false);
+            }
+            skill = false;
+        }
+        else
+        {
+            foreach (var obj in objectWhite)
+            {
+                obj.SetActive(false);
+            }
+
+            foreach (var obj in objectBlack)
+            {
+                obj.SetActive(true);
+            }
+            skill = true;
+        }
+    }
+
+    private void SelectObject()
+    {
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("White"))
+        {
+            objectWhite.Add(fooObj);
+        }
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("Black"))
+        {
+            objectBlack.Add(fooObj);
+        }
+    }
+
+}
---
+++ Assets/3_Scripts/Player1/Player1.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    //ATRIVUTOS Y PROPIEDADES           ************************************************ MATIAS BADALONI CESARINI ************************************************
+
+    //Objetos Primarios=
+    //Personales:
+    public GameObject playerCamera1;
+    private Rigidbody2D rb;
+    public ChangeStatesPlayer1 changeStates;
+    public GameObject body;
+    public Animator anim;
+    public AudioSource audioHabilidad;
+
+    //Externos:
+    public LevelManager levelManager;
+
+    //Estados Generales=
+    public bool active;
+    private float bodyx;
+
+    //Movimiento=
+    //A los lados:
+    private float moveHorizontal; //Direccion horizontal donde se movera el jugador (solo incluye derecha o izquierda)
+    private float speed; //Velocidad con que corre el jugador  
+    public List<string> controlMovement;
+
+    //Habilidad=
+    private bool keyUp;
+    private bool skill;
+
+    //METODOS PRIMARIOS //           ************************************************ MATIAS BADALONI CESARINI ************************************************
+
+    //Activar=
+    public void Active()
+    {
+        active = true;
+        playerCamera1.SetActive(true);
+    }
+    public void Desactive()
+    {
+        active = false;
+        playerCamera1.SetActive(false);
+
+    }
+
+    //General=
+    //Principal:
+    void Start()
+    {
+        //Objetos Primarios=
+        {
+            //Asignar objetos:
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        //Movimiento=
+        {
+            speed = 15;
+            controlMovement.Add ("a"); //0
+            controlMovement.Add ("d"); //1
+        }
+
+        //Habilidad
+        {
+            keyUp = true;
+        }
+
+        //Asignacion General=
+        {
+            //Llamado de Metodos:
+            if (active == true)
+                Active();
+            //Valor scala X body:
+            bodyx = body.transform.localScale.x;
+        }
+
+    }
+    void FixedUpdate()
+    {
+        if (active == true)
+        {
+            //Movimiento=
+            {
+                if (Input.GetKey(controlMovement[0]) && !Input.GetKey(controlMovement[1]) || Input.GetKey(controlMovement[1]) && !Input.GetKey(controlMovement[0]))
+                {
+                    anim.SetBool("Run", true);
+                    moveHorizontal = Input.GetAxis("Horizontal") * speed;
+                    rb.AddForce(Vector2.right * moveHorizontal, ForceMode2D.Impulse);
+                    if (rb.velocity.magnitude > speed)
+                        ForceReduced();
+                    //Animacion:
+                    if (moveHorizontal > 0)
+                    {
+                        body.transform.localScale = new Vector2(bodyx, body.transform.localScale.y);
+                    }
+                    if (moveHorizontal < 0)
+                    {
+                        body.transform.localScale = new Vector2(-bodyx, body.transform.localScale.y);
+                    }
+                }
+                else
+                {
+                    if (rb.velocity.magnitude > 0 || anim.GetBool ("Run"))
+                    {
+                        ForceReduced();
+                        Repose();
+                    }
+                }
+            }
+
+            //Habilidad= 
+            {
+                if (Input.GetKey("w"))
+                {
+                    if (keyUp == true)
+                    {
+                        keyUp = false;
+                        Skill();
+                    }
+                }
+                else
+                {
+                    if (keyUp == false)
+                        keyUp = true;
+                }
+            }
+        }
+    }
+
+    //Secundario:
+    //habilidad=
+    private void Skill()
+    {
+        changeStates.ChangeStates();
+        audioHabilidad.Play();
+    }
+
+    //Terciario:
+    //reposo=
+    private void Repose()
+    {
+        anim.SetBool("Run", false);
+    }
+    private void ForceReduced()
+    {
+        rb.AddForce(new Vector2(-rb.velocity.x, 0) * 1 * 0.2f, ForceMode2D.Impulse);
+    }
+    //muerte=
+    private void Dead()
+    {
+        active = false;
+        Repose();
+        rb.velocity = Vector2.zero;
+        anim.SetBool("Dead", true);
+    }
+    public void ResetLevel()
+    {
+        levelManager.Restart();
+    }
+
+    //collider=
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Collider2D>().tag == "Enemy")
+        {
+            Dead();
+        }
+    }
+
+}
