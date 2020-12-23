9f83d014af72b285ccf432cd5ea4ab3af32d3883
Matias Badaloni <53199112+MatiasBadaloni@users.noreply.github.com>
2020-12-23 11:48:19 -0300

    Detalles
    ...

---
+++ Assets/3_Scripts/Mecanismos/Piston.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston : MonoBehaviour
+{
+
+    public Animator anim;
+    public Puerta puerta;
+    public bool permanent;
+    private bool open;
+    public bool reverse;
+    public bool playNow;
+    public bool specialTransform;
+    public GameObject objectSpecial;
+
+    private void Start()
+    {
+        if (playNow == true)
+        {
+            puerta.Open();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (specialTransform == false) {
+            if (open == false)
+            {
+                open = true;
+                anim.SetBool("Open", true);
+                if (reverse == false)
+                {
+                    puerta.Open();
+                }
+                else
+                {
+                    puerta.Close();
+                }
+            }
+        }
+        else
+        {
+            objectSpecial.tag = "White";
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (specialTransform == false)
+        {
+            if (open == true)
+            {
+                if (permanent == false)
+                {
+                    open = false;
+                    anim.SetBool("Open", false);
+                    if (reverse == true)
+                    {
+                        puerta.Open();
+                    }
+                    else
+                    {
+                        puerta.Close();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/3_Scripts/Mecanismos/Puerta.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    public Animator anim;
+
+    public void Open()
+    {
+        anim.SetBool("Open", true);
+    }
+    public void Close()
+    {
+        anim.SetBool("Open", false);
+    }
+}
