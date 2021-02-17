f02cb813afd8f6d290d18878ee2298842b4db12f
Matias Badaloni <53199112+MatiasBadaloni@users.noreply.github.com>
2021-02-17 20:30:50 -0300

    Proyecto en pausa
    Este juego no debe ser tocado nunca mas... A menos que tenga tiempo libre. De otra forma, no debo gastar mas nada de mi en este error

--- Assets/3_Scripts/Win.cs
+++ Assets/3_Scripts/Win.cs
@@ -5,16 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Win : MonoBehaviour
 {
-    public int nextScene;
-    private GameObject canvasTextGB;
-    private AnimatedText canvasText;
-
    private void Start()
     {
         canvasTextGB = GameObject.FindWithTag("CanvasText");
         canvasText = canvasTextGB.GetComponent<AnimatedText>();
 
     }
+    public int nextScene;
+    private GameObject canvasTextGB;
+    private AnimatedText canvasText;
+
     private void NextLevel()
     {
         SceneManager.LoadScene(nextScene);
